041506d48293fef5c9fcfd61d376cfdccf34b7c1
Sergey Pererva <proger.su@gmail.com>
2016-07-08 22:25:48 +0300

    Update htaccess

--- app/core/model.php
+++ app/core/model.php
@@ -7,13 +7,14 @@ class Model extends medoo {
 	protected $db;
 
 	function __construct() {
-		$this->db = new Medoo([
+		$this->db = new Medoo(array(
 			'database_type' => 'mysql',
 			'database_name' => DB_NAME,
 			'server' => DB_HOST,
 			'username' => DB_USER,
 			'password' => DB_PASSWORD,
 			'charset' => DB_CHARSET
-		]);
+		));
 	}
+
 }
